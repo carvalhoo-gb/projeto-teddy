2f67e7dc2576c508663b4feec09c3899a8924bd3
Haufe <dev04@nodetech.com.br>
2024-09-09 16:04:06 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, Link, useLocation } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'primereact/resources/themes/saga-orange/theme.css';  // Tema específico
+// import 'primereact/resources/themes/tailwind-light/theme.css';
+import 'primereact/resources/themes/saga-orange/theme.css';
 import 'primereact/resources/primereact.min.css';          // Estilos primários do PrimeReact
 import 'primeicons/primeicons.css';                        // Ícones do PrimeIcons
-import "./App.css";                      
+import Cookies from 'js-cookie';
+import { Menubar } from 'primereact/menubar';
+import { Badge } from 'primereact/badge';
+import LogoTeddy from "./media/logo-teddy.png";
+import './App.css'
 
 import ListarParceiros from "./components/parceiros/listar-parceiros.component";
 import ListarEmpresasExternas from "./components/empresas-externas/listar-empresas-externas";
@@ -13,38 +17,55 @@ import Login from "./components/login/login";
 
 function App() {
   const location = useLocation();
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    // Tenta obter o nome de usuário do cookie
+    const user = Cookies.get('username');
+    if (user) {
+      setUsername(user);
+    }
+  }, []);
+
+  const itemRenderer = (item) => (
+    <a className="flex align-items-center p-menuitem-link">
+      <span className={item.icon} />
+      <span className="mx-2">{item.label}</span>
+      {item.badge && <Badge className="ml-auto" value={item.badge} />}
+      {item.shortcut && <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{item.shortcut}</span>}
+    </a>
+  );
+  const items = [
+    {
+      label: 'Parceiros',
+      url: '/ListarParceiros'
+    },
+    {
+      label: 'Empresas Externas',
+      url: '/ListarEmpresasExternas'
+    },
+    {
+      label: 'Sobre',
+      url: '/Sobre'
+    },
+    {
+      label: 'Sair',
+      url: '/' // Redireciona para a página de login
+    }
+  ];
+
+  const start = <img alt="logo" src={LogoTeddy} height="40" className="mr-2"></img>;
+
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <span className="mx-2">{username ? `Bem-vindo, ${username}` : ''}</span>
+    </div>
+  );
 
   return (
-    <div>
-      {/* Condicionalmente renderiza a Navbar apenas se o caminho não for "/login" */}
+    <div className="card">
       {location.pathname !== '/' && (
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/ListarParceiros"} className="navbar-brand">
-            Teddy
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/ListarParceiros"} className="nav-link">
-                Parceiros
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/ListarEmpresasExternas"} className="nav-link">
-                Empresas Externas
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/Sobre"} className="nav-link">
-                Sobre
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/"} className="nav-link">
-                Sair
-              </Link>
-            </li>
-          </div>
-        </nav>
+        <Menubar model={items} start={start} end={end} className="custom-menubar" />
       )}
 
       <div className="container mt-3">
@@ -56,7 +77,7 @@ function App() {
         </Routes>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
